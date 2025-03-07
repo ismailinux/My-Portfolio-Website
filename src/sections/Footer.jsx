ed666fb664265d2384ff6d05e78b9c6009f9c3df
Ismailenox <ismailenox@gmail.com>
2025-03-07 15:00:51 +0100

    minor changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -14,13 +14,13 @@ const Footer = () => {
           </a>
         </div>
         <div className="social-icon">
-          <a href="https://x.com/MonsieurFemi" className="flex items-center justify-center" target="_blank">
-             <img src="/assets/X-logo.png" alt="twitter" className="w-1/2 h-1/2" />
+          <a href="https://www.linkedin.com/in/ismail-opaluwa-74a016236/" className="flex items-center justify-center" target="_blank" >
+             <img src="/assets/linkedin.png" alt="linkedin" className="w-1/2 h-1/2" />
           </a>
         </div>
         <div className="social-icon">
-          <a href="https://www.instagram.com/riseofguy" className="flex items-center justify-center" target="_blank" >
-             <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
+          <a href="https://x.com/MonsieurFemi" className="flex items-center justify-center" target="_blank">
+             <img src="/assets/X-logo.png" alt="twitter" className="w-1/2 h-1/2" />
           </a>
         </div>
       </div>
